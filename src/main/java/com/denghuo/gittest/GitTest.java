f217a3a9f3725975b742ee7da476b872362ce126
root <614064125@qq.com>
2022-08-18 09:17:04 +0800

    push commit2

--- src/main/java/com/denghuo/gittest/GitTest.java
+++ src/main/java/com/denghuo/gittest/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix-test!!!!master-test!!!");
         System.out.println("hot-fix-test!!!!!!!Change");
         System.out.println("push-test");
+        System.out.println("push-test2");
 
     }
 }
